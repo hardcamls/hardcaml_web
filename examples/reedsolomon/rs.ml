1797fe14652aab0d84d4a14fa7003f1a61886d0b
andrewray <evilkidder@gmail.com>
2022-11-23 18:14:39 +0000

    add reedsolomon decoder testbench

--- examples/reedsolomon/rs.ml
+++ examples/reedsolomon/rs.ml
@@ -1,5 +1,6 @@
 open! Base
 open! Hardcaml
+open Hardcaml_waveterm
 open Hardcaml_web
 
 let title = "Hardcaml Reed-solomon Decoder"
@@ -20,6 +21,25 @@ module Make (P : Parameters.S) = struct
   let k = n - (2 * t)
   let b = 0
   let m = Int.ceil_log2 (n + 1)
+
+  module Gp = struct
+    let pp =
+      Array.foldi Reedsolomon.Galois.gf2_prim_polys.(m) ~init:0 ~f:(fun idx acc b ->
+        acc + if b = 0 then 0 else b lsl idx)
+    ;;
+
+    let pe = 2
+  end
+
+  module Rp = struct
+    let k = k
+    let b = b
+    let t = t
+  end
+
+  module Parallelism = struct
+    let n = parallelism
+  end
 
   module Codec =
     Hardcaml_reedsolomon.Codec.Make
@@ -45,5 +65,25 @@ module Make (P : Parameters.S) = struct
   module O = Decoder.O
 
   let create = Decoder.create
-  let testbench = None
+
+  module Test =
+    Test_hardcaml_reedsolomon.Test_decoder.Test
+      (Reedsolomon.Standards.Make (Gp) (Rp)) (Parallelism)
+
+  let testbench () =
+    let waves = Test.test ~waves:true () in
+    let rules =
+      let module I = Display_rules.With_interface (I) in
+      let module O = Display_rules.With_interface (O) in
+      [ I.default ~wave_format:(Bit_or Int) (); O.default ~wave_format:(Bit_or Int) () ]
+      |> List.concat
+    in
+    Testbench_result.of_waves
+      ~options:
+        { display_width = 160; display_height = 30; start_cycle = 0; wave_width = 0 }
+      ~rules
+      (Option.value_exn waves)
+  ;;
+
+  let testbench = Some testbench
 end
