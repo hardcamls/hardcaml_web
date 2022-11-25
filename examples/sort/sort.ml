964628ea29a2e906003c949da5296a7bcebdbd43
andrewray <evilkidder@gmail.com>
2022-11-25 00:31:01 +0000

    sorting network

--- examples/sort/sort.ml
+++ examples/sort/sort.ml
@@ -5,26 +5,64 @@ open Hardcaml_web
 
 let title = "no title yet"
 let top_level_name = "no_name"
-let default_parameters = []
+
+let default_parameters =
+  Parameter.
+    [ "data_width", { typ = Int 8; description = "data width" }
+    ; "size", { typ = Int 4; description = "network size" }
+    ; ( "type"
+      , { typ =
+            Symbol
+              { options =
+                  List.map Hardcaml_circuits.Sorting_network.Config.all ~f:(fun t ->
+                    Hardcaml_circuits.Sorting_network.Config.sexp_of_t t
+                    |> Sexp.to_string
+                    |> String.map ~f:(fun c ->
+                         match Char.lowercase c with
+                         | '_' -> '-'
+                         | c -> c))
+              ; value = 0
+              }
+        ; description = "network type"
+        } )
+    ]
+;;
 
 module Make (P : Parameters.S) = struct
+  let data_width = Parameters.as_int_exn P.parameters "data_width"
+  let size = Parameters.as_int_exn P.parameters "size"
+
+  let network_type =
+    let n = Parameters.as_symbol_exn P.parameters "type" in
+    List.nth_exn Hardcaml_circuits.Sorting_network.Config.all n.value
+  ;;
+
   module I = struct
-    type 'a t = { d : 'a } [@@deriving sexp_of, hardcaml]
+    type 'a t = { d : 'a list [@bits data_width] [@length size] }
+    [@@deriving sexp_of, hardcaml]
   end
 
   module O = struct
-    type 'a t = { q : 'a } [@@deriving sexp_of, hardcaml]
+    type 'a t = { q : 'a list [@bits data_width] [@length size] }
+    [@@deriving sexp_of, hardcaml]
   end
 
   open Signal
 
-  let create _scope (i : _ I.t) = { O.q = ~:(i.d) }
+  let compare_and_swap a b =
+    let lt = a <: b in
+    { Hardcaml_circuits.Sorting_network.Min_max.min = mux2 lt a b; max = mux2 lt b a }
+  ;;
+
+  let create _scope (i : _ I.t) =
+    { O.q = Hardcaml_circuits.Sorting_network.create network_type compare_and_swap i.d }
+  ;;
 
   let testbench (sim : (Bits.t ref I.t, Bits.t ref O.t) Cyclesim.t) =
     let inputs = Cyclesim.inputs sim in
     let waves, sim = Waveform.create sim in
-    for i = 0 to 5 do
-      inputs.d := Bits.of_bool (i % 2 = 0);
+    for _ = 1 to 10 do
+      List.iter inputs.d ~f:(fun d -> d := Bits.random ~width:data_width);
       Cyclesim.cycle sim
     done;
     let rules =
@@ -35,7 +73,11 @@ module Make (P : Parameters.S) = struct
     in
     Testbench_result.of_waves
       ~options:
-        { display_width = 100; display_height = 30; start_cycle = 0; wave_width = 2 }
+        { display_width = 100
+        ; display_height = 5 + (2 * 3 * size)
+        ; start_cycle = 0
+        ; wave_width = 2
+        }
       ~rules
       waves
   ;;
