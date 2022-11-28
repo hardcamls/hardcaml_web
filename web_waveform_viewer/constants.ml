e06ec42fde016798d22d3095615773381d823d8d
fyquah <fyquah@protonmail.com>
2022-11-28 12:28:58 +0000

    Align canvas better

--- web_waveform_viewer/constants.ml
+++ web_waveform_viewer/constants.ml
@@ -1,5 +1,5 @@
-let x_offset_to_start_of_signal = 2.0
-let y_offset_to_start_of_signal = 2.0
 let canvas_scaling_factor = 2
+let x_offset_to_start_of_signal = 2.0
+let y_offset_to_start_of_signal = 6.86 *. Float.of_int canvas_scaling_factor
 let signal_line_width = Float.of_int (1 * canvas_scaling_factor)
 let font_size_in_pixels = 12 * canvas_scaling_factor
